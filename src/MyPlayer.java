94453b701f26ebdab6871801d3699abff54c756c
Philip Mikhailov <pdmikhailov@gmail.com>
2025-01-21 14:15:54 -0500

    1/21

--- src/MyPlayer.java
+++ src/MyPlayer.java
@@ -1,95 +1,58 @@
 import java.awt.*;
-import java.sql.SQLOutput;
 import java.util.Arrays;
 
 public class MyPlayer {
     public Chip[][] gameBoard;
     public int[] columns;
-
     public Board[] losingBoards = new Board[10];
     public Board[] winningBoards = new Board[20];
 
+    public int losingBoardCount = 0;
+    public int winningBoardCount = 0;
 
     public MyPlayer() {
-        columns = new int[10];
-
-
-        /***
-         * This code will run just once, when the game opens.
-         * Add your code here.
-         */
+        columns = new int[3];
+        //losingBoards (1, 0, 0, 0, 0)
+//        first losing board
         print3x3boards();
-        //fill columns array with 0's
-
-        Board b = new Board(1,0,0, 0,0);
-        losingBoards[0] = b;
-
-
     }
 
     public Point move(Chip[][] pBoard) {
-
-        System.out.println("MyPlayer Move");
-
-
-
-
+        //System.out.println("MyPlayer Move");
         gameBoard = pBoard;
 
-        int column = 0;
         int row = 0;
-
-        row = 1;
-        column = 1;
-
-        /***
-         * This code will run each time the "MyPlayer" button is pressed.
-         * Add your code to return the row and the column of the chip you want to take.
-         * You'll be returning a data type called Point which consists of two integers.
-         */
-        //6,5,4,3,2,1,0
-        //cant have 4,6,1 has to be in descending order
-        //Starts off with 10,10,10,10,10,10,10,10,10,10
-
-        // add code to fill in the column array
-        //so that it represents the gameBoard in number form
-        // print your columns array to show it's working
-
-        System.out.println("MY PLAYER CLICKED");
+        int column = 0;
 
         for (int c = 0; c < columns.length; c++) {
             int count = 0;
             for (int r = 0; r < gameBoard.length; r++) {
-                if (gameBoard[r][c].isAlive == true) {
+                if (gameBoard[r][c].isAlive) {
                     count++;
                 }
             }
-
             columns[c] = count;
-
-        }
-        System.out.println(Arrays.toString(columns));
-
-        Point myMove;
-        myMove = new Point (row, column);
-
-        if (columns[0] == 3 && columns[1] == 3 && columns[2] ==0){
-            myMove = new Point(2,1);
         }
 
+        // Compare current board with known winning boards/moves
+        for (int i = 0; i < winningBoardCount; i++) {
+            if (Arrays.equals(columns, winningBoards[i].cols)) {
+                row = winningBoards[i].winRow;
+                column = winningBoards[i].winCol;
+                //System.out.println("Winning Move: (" + row + ", " + column + ")");
+
+            }
+        }
+
+        Point myMove = new Point(row, column);
         return myMove;
     }
 
     public void print3x3boards() {
-        // using 1 or more for loops, print all possible 3x3 or smaller board states
-        //figure out how to cancel out the impossble boards
         for (int i = 1; i <= 3; i++) {
             for (int j = 0; j <= 3; j++) {
                 for (int k = 0; k <= 3; k++) {
-                    //homework
-                    if (i >= j && j>=k) {
-                        //System.out.println(i + " " + j + " " + k);
-                        System.out.println("   ");
+                    if (i >= j && j >= k) {
                         oneMove(i, j, k);
                     }
                 }
@@ -97,83 +60,88 @@ public class MyPlayer {
         }
     }
 
-    public void oneMove(int i, int j, int k){
-        System.out.println("Possible moves from:"+ i + "," + j + "," + k );
-
+    public void oneMove(int i, int j, int k) {
+        //System.out.println("Main Board: " + i + "," + j + "," + k);
         boolean isWinningBoard = false;
+        int winningMoveRow = -1;
+        int winningMoveCol = -1;
 
-//        int newK = k-1;
-//        int newJ = j-1;
-//        int newI = i-1;
-        //OOX
-        //OOX
-        //OOO
+        //right
+        for (int newK = k - 1; newK >= 0; newK--) {
+            // System.out.println("New Board (Right): " + i + "," + j + "," + newK + " Move to get to the Board:(" + newK + ", 2)");
 
-        for (int newK = k-1; newK >= 0; newK--){
-            System.out.println("New Board (Right):" + i + "," + j + "," + newK + " Move to get to the Board:(" + (newK) + "," + 2 + ")");
-            // if i j newK == cols array of a losing board then flip boolean to true
-//            System.out.println("Move to get to the Board:(" + (newK) + "," + 2 + ")");
-
-//losingBoards = array, losingBoards[0] = Board[0], losingBoard[0].cols = int [], losingBoard[0].cols[0] = int
-        }
-
-        for (int newJ = j-1; newJ >=0; newJ--){
-            if(newJ >= k){
-
-                System.out.println("1 New Board (Middle):" + i + "," + newJ + "," + k + " Move to get to the board: (" + (newJ) + "," + 1 + ")");
-
-
-            } else {
-                System.out.println("2 New Board (Middle):" + i + "," + newJ + "," + newJ + " Move to get to the Board:(" + (newJ) + "," + 1 + ")");
-
+            if (isLosingBoard(i, j, newK)) {
+                isWinningBoard = true;
+                winningMoveRow = newK;
+                winningMoveCol = 2;
             }
         }
-        for (int newI = i-1; newI >=1; newI--){
-            if (newI>=j && newI>=k){
-                System.out.println("1 New Board (Left):" + newI + "," + j + "," + k + " Move to get to the Board:(" + (newI) + "," + 0 +")");
+        //midle
+        for (int newJ = j - 1; newJ >= 0; newJ--) {
 
-
-            } else if (newI<j && newI>=k) {
-                System.out.println("2 New Board (Left):" + newI + "," + newI + "," + k + " Move to get to the Board:(" + (newI) + "," + 0 +")");
-
-            } else {
-                System.out.println("3 New Board (Left):" + newI + "," + newI + "," + newI + " Move to get to the Board:(" + (newI) + "," + 0 + ")");
-
+            if (newJ >= k && isLosingBoard(i, newJ, k)) {
+                //System.out.println("1New Board (Middle): " + i + "," + newJ + "," + k + " Move to get to the Board:(" + newJ + ", 1)");
+                isWinningBoard = true;
+                winningMoveRow = newJ;
+                winningMoveCol = 1;
+            } else if (newJ < k && isLosingBoard(i, newJ, newJ)) {
+                //System.out.println("2New Board (Middle): " + i + "," + newJ + "," + k + " Move to get to the Board:(" + newJ + ", 1)");
+                isWinningBoard = true;
+                winningMoveRow = newJ;
+                winningMoveCol = 1;
             }
+        }
+        //left
+        for (int newI = i - 1; newI >= 1; newI--) {
 
+            if (newI >= j && newI >= k && isLosingBoard(newI, j, k)) {
+                // System.out.println("1New Board (Left): " + newI + "," + j + "," + k + " Move to get to the Board:(" + newI + ", 0)");
+                isWinningBoard = true;
+                winningMoveRow = newI;
+                winningMoveCol = 0;
 
+            } else if (newI < j && newI >= k) {
+                // System.out.println("2New Board (Left): " + newI + "," + j + "," + k + " Move to get to the Board:(" + newI + ", 0)");
+                if (isLosingBoard(newI, newI, k)) {
+                    isWinningBoard = true;
+                    winningMoveRow = newI;
+                    winningMoveCol = 0;
+                    break;
+                }
+            } else {
+                if (newI < j && newI < k && isLosingBoard(newI, newI, newI)) {
+                    // System.out.println("3New Board (Left): " + newI + "," + j + "," + k + " Move to get to the Board:(" + newI + ", 0)");
+                    isWinningBoard = true;
+                    winningMoveRow = newI;
+                    winningMoveCol = 0;
+                }
+            }
         }
 
-
-//         if (newI<newK && newJ<newK && newI<newJ){
-//             System.out.println("Impossible board");
-//         }
-        // add board (i,j,k, row, column) to either
-        // the "Winners" array or the "losers" array
-        // based on isWinningBoard boolean
-
+        //final descion on winning or losing board
+        if (isWinningBoard) {
+            storeBoard(new Board(i, j, k, winningMoveRow, winningMoveCol), true);
+        } else {
+            storeBoard(new Board(i, j, k, 0, 0), false);
+        }
     }
-
+    //check if ijk already losing board
+    public boolean isLosingBoard(int i, int j, int k) {
+        for (int x = 0; x < losingBoardCount; x++) {
+            if (losingBoards[x].cols[0] == i && losingBoards[x].cols[1] == j && losingBoards[x].cols[2] == k) {
+                return true;
+            }
+        }
+        return false;
+    }
+    //storing board
+    public void storeBoard(Board board, boolean isWinningBoard) {
+        if (isWinningBoard) {
+            winningBoards[winningBoardCount++] = board;
+            //System.out.println("Winning board: " + Arrays.toString(board.cols));
+        } else {
+            losingBoards[losingBoardCount++] = board;
+            //System.out.println("Losing board: " + Arrays.toString(board.cols));
+        }
+    }
 }
-
-
-
-
-
-
-//    if (columns[0] == 3 && columns[1] == 3 && columns[2] ==0){
-//myMove = new Point(2,1);
-//        }
-
-
-
-
-
-
-
-
-
-
-
-
-
